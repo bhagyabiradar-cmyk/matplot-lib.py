f435da267d224d0f6254c9b927e7728592f5a3a6
Bhagyashree Biradar <bhagyabiradar63@gmail.com>
2026-08-18 18:26:13 +0530

    Add files via upload

---
+++ scikit learn.py
@@ -0,0 +1,17 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+# Training data
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([2, 4, 6, 8, 10])
+
+# Create model
+model = LinearRegression()
+
+# Train model
+model.fit(X, y)
+
+# Predict
+prediction = model.predict([[6]])
+
+print("Prediction:", prediction[0])
